b9457fc7809e1b8caadc2e5f521858bae22261cf
molecule-lab-rushil <rushildhinoja@moleculelab.in>
2025-06-22 20:40:46 +0530

    Dev replacing api to localhost

--- apps/widget/src/api/axios-instance.js
+++ apps/widget/src/api/axios-instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getBaseURL = () => {
-  return "https://localhost:3001/api/v1";
+  return "http://localhost:3001/api/v1";
 };
 
 let currentTenantId = null;
